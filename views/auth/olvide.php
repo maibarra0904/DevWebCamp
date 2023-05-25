63b397d0b96c5383e2d7c0ddf1724b1ed7d8e54d
maibarra0904 <mario.ibarra.86@gmail.com>
2023-05-25 10:39:45 -0500

    250523

--- views/auth/olvide.php
+++ views/auth/olvide.php
@@ -1,6 +1,6 @@
 <main class="auth">
     <h2 class="auth__heading"> <?php echo $titulo; ?></h2>
-    <p class="auth__texto">Inicia sesión en DevWebcamp</p>
+    <p class="auth__texto">Recupera tu acceso a DevWebcamp</p>
 
 
     <form action="formulario">
@@ -9,17 +9,12 @@
             <input type="email" class="formulario__input" placeholder="Tu email" id="email" name="email">
         </div>
 
-        <div class="formulario__campo">
-            <label class="formulario__label">Password</label>
-            <input type="password" class="formulario__input" placeholder="Tu password" id="password" name="password">
-        </div>
-
-        <input type="submit" class="formulario__submit" value="Iniciar Sesión">
+        <input type="submit" class="formulario__submit" value="Enviar instrucciones">
     </form>
 
     <div class="acciones">
+        <a href="/login" class="acciones__enlace">¿Ya tienes cuenta? Inicia Sesión</a>
         <a href="/registro" class="acciones__enlace">¿Aún no tienes cuenta? Crea una</a>
-        <a href="/olvide" class="acciones__enlace">¿Olvidaste tu password?  Recupera tu cuenta</a>
     </div>
 
 </main>
