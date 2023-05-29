e1f9359b79fa8c3a1fe4a72c1f0f11ca547a530a
maibarra0904 <mario.ibarra.86@gmail.com>
2023-05-29 08:49:06 -0500

    290523

--- controllers/PonentesController.php
+++ controllers/PonentesController.php
@@ -6,12 +6,18 @@ use MVC\Router;
 
 class PonentesController {
     public static function index(Router $router) {
-        // if($_SERVER['REQUEST_METHOD'] === 'POST') {
-
-        // }
-
         $router->render('admin/ponentes/index', [
             'titulo' => 'Ponentes/Conferencistas',
+        ]);
+       
+    }
+
+    public static function crear(Router $router) {
+        $alertas = [];
+
+        $router->render('admin/ponentes/crear', [
+            'titulo' => 'Registrar Ponente',
+            'alertas' => $alertas
         ]);
        
     }
