f9c8b8b93e49572f9949a0cf191ec837d6ede994
maibarra0904 <mario.ibarra.86@gmail.com>
2023-06-14 07:18:44 -0500

    140623

--- controllers/PaginasController.php
+++ controllers/PaginasController.php
@@ -42,16 +42,21 @@ class PaginasController {
         }
 
         //Obtener el total de cada bloque
-        $ponentes = Ponente::total();
-        $conferencias = Evento::total('categoria_id',1);
-        $workshops = Evento::total('categoria_id',2);
+        $ponentes_total = Ponente::total();
+        $conferencias_total = Evento::total('categoria_id',1);
+        $workshops_total = Evento::total('categoria_id',2);
         
+        //Obtener todos los ponentes
+        $ponentes = Ponente::all();
+
         $router->render('paginas/index', [
             'titulo' => 'Inicio',
             'eventos' => $eventos_formateados,
+            'ponentes_total' => $ponentes_total,
+            'conferencias_total' => $conferencias_total,
+            'workshops_total' => $workshops_total,
             'ponentes' => $ponentes,
-            'conferencias' => $conferencias,
-            'workshops' => $workshops
+            'titulo_ponentes' => 'Speakers'
         ]);
     }
 
