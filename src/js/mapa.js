14266370dd3abb6be23cf4a465a1fca238956dfe
maibarra0904 <mario.ibarra.86@gmail.com>
2023-06-14 22:23:25 -0500

    140623

---
+++ src/js/mapa.js
@@ -0,0 +1,19 @@
+if(document.querySelector('#mapa')) {
+
+    const lat = -2.1226773686628846;
+    const lng = -79.59924638804313;
+    const zoom = 16;
+
+    const map = L.map('mapa').setView([lat, lng], zoom);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    L.marker([lat, lng]).addTo(map)
+        .bindPopup(`
+            <h2 class="mapa__heading">DevWebCamp</h2>
+            <p class="mapa__texto">Centro de Operaciones</p>
+        `)
+        .openPopup();
+}
--- views/paginas/index.php
+++ views/paginas/index.php
@@ -95,3 +95,36 @@
 
     </div>
 </section>
+
+<div class="mapa" id="mapa">
+
+</div>
+
+<section>
+    <h2 class="boletos__heading">Boletos & Precios</h2>
+    <p class="boletos__descripcion">Precios para DevWebCamp</p>
+
+    <div class="boletos__grid">
+        <div class="boleto boleto--presencial">
+            <h4 class="boleto__logo">&#60;DevWebCamp /></h4>
+            <p class="boleto__plan">Presencial</p>
+            <p class="boleto__precio">$199</p>
+        </div>
+
+        <div class="boleto boleto--virtual">
+            <h4 class="boleto__logo">&#60;DevWebCamp /></h4>
+            <p class="boleto__plan">Virtual</p>
+            <p class="boleto__precio">$49</p>
+        </div>
+
+        <div class="boleto boleto--gratis">
+            <h4 class="boleto__logo">&#60;DevWebCamp /></h4>
+            <p class="boleto__plan">Gratis</p>
+            <p class="boleto__precio">$0</p>
+        </div>
+
+        <div class="boleto__enlace-contenedor">
+            <a href="/paquetes" class="boleto__enlace">Ver Paquetes</a>
+        </div>
+    </div>
+</section>
