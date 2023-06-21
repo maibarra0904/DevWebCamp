6fcedb9861c74c305420f3200f1f8405d1ae1227
maibarra0904 <mario.ibarra.86@gmail.com>
2023-06-20 22:46:31 -0500

    200623

--- controllers/RegistroController.php
+++ controllers/RegistroController.php
@@ -11,6 +11,7 @@ use Model\Ponente;
 use Model\Usuario;
 use Model\Registro;
 use Model\Categoria;
+use Model\Regalo;
 
 class RegistroController {
     public static function crear(Router $router) {
@@ -168,10 +169,13 @@ class RegistroController {
                 $eventos_formateados['workshops_s'][] = $evento;
             }
         }
+            
+        $regalos = Regalo::all('ASC');
 
         $router->render('registro/conferencias', [
             'titulo' => 'Elige WorkShops y Conferencias',
-            'eventos' => $eventos_formateados
+            'eventos' => $eventos_formateados,
+            'regalos' => $regalos
         ]);
        
     }
--- views/registro/conferencias.php
+++ views/registro/conferencias.php
@@ -7,9 +7,33 @@
         <h3 class="eventos-registro__heading--conferencias">&lt;Conferencias /></h3>
         <p class="eventos-registro__fecha">Viernes 5 de Octubre</p>
 
-        <?php //debuguear($eventos) ?>;
         <div class="eventos-registro__grid">
             <?php foreach($eventos['conferencias_v'] as $evento) {
+                include __DIR__ . '/evento.php';
+            }; ?>
+        </div>
+
+        <p class="eventos-registro__fecha">Sabado 10 de Octubre</p>
+
+        <div class="eventos-registro__grid">
+            <?php foreach($eventos['conferencias_s'] as $evento) {
+                include __DIR__ . '/evento.php';
+            }; ?>
+        </div>
+
+        <h3 class="eventos-registro__heading--workshops">&lt;WorkShops /></h3>
+        <p class="eventos-registro__fecha">Viernes 5 de Octubre</p>
+
+        <div class="eventos-registro__grid eventos--workshops">
+            <?php foreach($eventos['workshops_v'] as $evento) {
+                include __DIR__ . '/evento.php';
+            }; ?>
+        </div>
+
+        <p class="eventos-registro__fecha">Sabado 10 de Octubre</p>
+
+        <div class="eventos-registro__grid eventos--workshops">
+            <?php foreach($eventos['workshops_s'] as $evento) {
                 include __DIR__ . '/evento.php';
             }; ?>
         </div>
@@ -17,6 +41,24 @@
 
     <aside class="registro">
             <h2 class="registro__heading">Tu Registro</h2>
+
+            <div id="registro-resumen" class="registro__resumen"></div>
+
+            <div class="registro__regalo">
+                <label for="regalo" class="registro__label">Selecciona un regalo</label>
+                <select id="regalo" class="registro__select">
+                    <option value="">-- Selecciona tu regalo --</option>
+                    <?php foreach($regalos as $regalo) { ?>
+                        <option value="<?php echo $regalo->id; ?>"><?php echo $regalo->nombre; ?></option>
+                    <?php } ?>
+                </select>
+            </div>
+
+            <form id="registro" class="formulario">
+                <div class="formulario__campo">
+                    <input type="submit" class="formulario__submit formulario__submit--full" value="Registrarme">
+                </div>
+            </form>
     </aside>
 </div>
     
